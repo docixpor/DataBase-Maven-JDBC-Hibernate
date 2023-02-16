a4255c102b56cc32a6021d60f335b9a42800e096
docixpor <119433532+docixpor@users.noreply.github.com>
2023-02-16 21:01:01 +0300

    load...

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -30,34 +30,34 @@ public class Util {
         return connection;
     }
 
-        public static SessionFactory getSessionFactory() {
-            if (sessionFactory == null) {
-                try {
-                    Configuration configuration = new Configuration();
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
-                    Properties settings = new Properties();
-                    settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
 
-                    settings.put(Environment.URL, URL);
-                    settings.put(Environment.USER, login);
-                    settings.put(Environment.PASS, password);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, login);
+                settings.put(Environment.PASS, password);
 
-                    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                    settings.put(Environment.SHOW_SQL, "true");
-                    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                    settings.put(Environment.HBM2DDL_AUTO, "");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
 
-                    configuration.setProperties(settings);
-                    configuration.addAnnotatedClass(User.class);
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
 
-                    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                            .applySettings(configuration.getProperties()).build();
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            return sessionFactory;
         }
+        return sessionFactory;
+    }
 }
